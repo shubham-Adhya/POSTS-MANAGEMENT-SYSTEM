b2e65289e661abcd97a00c0dfd7f414e19062e6a
shubham-Adhya <119453418+shubham-Adhya@users.noreply.github.com>
2023-03-27 16:35:20 +0530

    posts update delete route

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -33,6 +33,42 @@ postRouter.get("/",async(req,res)=>{
     }
 })
 
+
+//update
+postRouter.patch("/update/:postID",async(req,res)=>{
+    const token=req.headers.authorization.split(" ")[1]
+    const decoded= jwt.verify(token,'masai')
+    const postID=req.params.postID
+    const payload=req.body;
+    try {
+        if(decoded){
+            await PostModel.findByIdAndUpdate({_id:postID,userID:decoded.userID},payload)
+            res.status(200).json({"msg":"Post has been updated"})
+        }else{
+            res.status(400).json({"msg":"Authorization Failed !"})
+        }
+    } catch (error) {
+        res.status(400).json({"msg":error.message}) 
+    }
+})
+
+//delete
+postRouter.delete("/delete/:postID",async(req,res)=>{
+    const token=req.headers.authorization.split(" ")[1]
+    const decoded= jwt.verify(token,'masai')
+    const postID=req.params.postID
+    try {
+        if(decoded){
+            await PostModel.findOneAndDelete({_id:postID,userID:decoded.userID})
+            res.status(200).json({"msg":"Post has been deleted"})
+        }else{
+            res.status(400).json({"msg":"Authorization Failed !"})
+        }
+    } catch (error) {
+        res.status(400).json({"msg":error.message}) 
+    }
+})
+
 module.exports={
     postRouter 
 }
