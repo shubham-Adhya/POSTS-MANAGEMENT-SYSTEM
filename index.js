4f5319cb3afade6a758413bb1fa31e22b1d135ed
shubham-Adhya <119453418+shubham-Adhya@users.noreply.github.com>
2023-03-28 02:35:09 +0530

    cors late_submit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { auth } = require("./middlewares/auth.middleware")
 
 const app=express()
 app.use(express.json())
-// app.use(cors());
+app.use(cors());
 
 app.get("/",(req,res)=>{
     res.status(200).send("Welcome to Posts Management System")
