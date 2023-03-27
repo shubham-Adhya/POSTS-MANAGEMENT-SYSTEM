bfa25b5ff4ee9ef72a70aba1c243499551b2c39f
shubham-Adhya <119453418+shubham-Adhya@users.noreply.github.com>
2023-03-27 14:34:15 +0530

    mvc connection and model created

---
+++ index.js
@@ -0,0 +1,29 @@
+const express=require("express")
+
+require("dotenv").config()
+const cors=require("cors");
+const { connection } = require("./connection/db");
+
+const app=express();
+app.use(cors());
+
+
+// app.use("/users",userRoute)
+
+
+
+app.get("/",(req,res)=>{
+    res.status(200).send("Welcome to Posts Management System")
+})
+
+
+app.listen(process.env.port,async()=>{
+    try {
+        await connection
+        console.log("Connected to DB")
+    } catch (error) {
+        console.log("Can not Connect to DB")
+        console.log(error)
+    }
+    console.log(`Server is running at PORT ${process.env.port}`)
+})
